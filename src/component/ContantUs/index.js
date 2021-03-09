3b8e53818d178fe64b6a817ea012939d0334eb6e
Francisc0HLL <j.francisco.hll@gmail.com>
2021-03-09 17:58:42 -0300

    se agrega pagina de contacto con sus estilos y formulario

--- src/component/ContantUs/index.js
+++ src/component/ContantUs/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Container, Form, FormButton, FormContent, FormInput, FormWrap, Icon , FormH1, FormLabel} from './contactusElements'
 
 const ContactUsPage = () => {
     return (
-        <div>
-            <h1>prueba de contacto</h1>
-        </div>
+        <>
+           <Container>
+                <FormWrap>
+                    <Icon to="/">OSL</Icon>
+                    <FormContent>
+                        <Form action="#">
+                            <FormH1>Formulario de Contacto</FormH1>
+                            <FormLabel htmlFor='for'>Nombres</FormLabel>
+                            <FormInput type='text' required/>
+                            <FormLabel htmlFor='for'>Email</FormLabel>
+                            <FormInput type='email' required/>
+                            <FormLabel htmlFor='for'>Celular</FormLabel>
+                            <FormInput type='text' required/>
+                            <FormButton type='submit'>Enviar</FormButton>
+                        </Form>
+                    </FormContent>
+                </FormWrap>   
+            </Container> 
+        </>
     )
 }
 
